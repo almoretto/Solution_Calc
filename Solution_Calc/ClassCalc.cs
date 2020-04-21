837c89831924df44567483e3059272f76968f0a4
André Luís Moretto <amoretto@outlook.com.br>
2020-04-21 09:16:06 -0300

    Calculadora

---
+++ Solution_Calc/ClassCalc.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution_Calc
+{
+    class ClassCalc
+    {
+        private double Resultado;
+        public double Somar(double a, double b)
+        {
+            return a + b;
+
+        }
+        public string Subtrair(double a, double b)
+        {
+            Resultado = a - b;
+            return Resultado.ToString("F2");
+        }
+        public string Multiplicar(double a, double b)
+        {
+            Resultado = a * b;
+            return Resultado.ToString("F2");
+        }
+        public string Dividir (double a, double b)
+        {
+            if (b == 0)
+            {
+                return "Não existe divisão por 0";
+            }
+            else
+            {
+                Resultado = a / b;
+                return Resultado.ToString("F2");
+            }
+        }
+        public string VerificaDivisor(double a, double b)
+        {
+            if (b == 0)
+            {
+                return "Não existe divisão por 0";
+            }
+            else
+            {
+                Resultado = a % b;
+                return Resultado.ToString("F2");
+            }
+        }
+        public string 
+    }
+}
---
+++ Solution_Calc/calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Solution_Calc
+{
+    class calculator
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
